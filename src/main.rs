9126a519bc8d0ccfa166e1a2910280d34d9c0623
Eichenour <R.D.Eichenour@gmail.com>
2026-06-17 18:32:04 -0500

    feat(wip): Create foundation for a create command
    Signed-off-by: Eichenour <R.D.Eichenour@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+use uuid::Uuid;
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Command to create a recipe given the file path and name
+    Create {
+        /// Filepath the resulting recipe file will be written to
+        #[arg(long)]
+        output_filepath: PathBuf,
+        /// Name of the recipe to create
+        #[arg(long)]
+        recipe_name: PathBuf,
+    },
+}
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(long, default_value = ".")]
+    root: PathBuf,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", Uuid::new_v4());
+
+    let args = Cli::parse();
+
+    match &args.command {
+        Some(Commands::Create {
+            output_filepath,
+            recipe_name,
+        }) => {
+            println!("outdir: {:?}, name: {:?}", output_filepath, recipe_name);
+        }
+        None => {
+            println!("Invalid command");
+        }
+    }
 }
