83d1ca0d24f46b5aa7b9203a628be1ac85a07b77
Eichenour, Riley <R.D.Eichenour@gmail.com>
2026-08-09 17:43:54 -0500

    fix: Update typo in ingredient creation
    Signed-off-by: Eichenour, Riley <R.D.Eichenour@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -140,7 +140,7 @@ fn main() {
                     }
                     CreateOption::AddIngredient => {
                         let ingredient_id = prompt_non_empty_string(
-                            "Enter ingreident id (lowercase, no whitespace):".to_string(),
+                            "Enter ingriedent id (lowercase, no whitespace):".to_string(),
                             true,
                         );
                         let ingredient_title =
